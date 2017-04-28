e86829b2e6883cc9ac341645f8f73ecca4a579f6
chrisbrickey <cbrickey1@gmail.com>
2017-04-27 20:41:04 -0700

    playing around

--- bin/mygame.rb
+++ bin/mygame.rb
@@ -5,12 +5,21 @@ set :static, true
 set :public_folder, "static"
 set :views, "views"
 
-#each of these blocks defines a ROUTE (e.g. get '/') and how the app responds to that specific HTTP request (e.g. "Hello world")
+#each of these blocks (aka 'handlers') defines a ROUTE (e.g. get '/') and how the app responds to that specific HTTP request (e.g. "Hello world")
+#'/' is called the 'index mapping'; when someone goes to this URL, Sinatra will find the class index and load it to handle the request
 get '/' do
   return 'Hello world'
 end
 
+#this 'handler' specifies what happens when the browser goes to /hello/
 get '/hello/' do
+
+  #this gets the <%= greeting %> (used in index.erb) from the params Hashmap...when you type "http://localhost:8080/hello/greeting=Hi" into the browser, this assigns greeting = "Hi"
+  # || means that if there's no params given (e.g. if I just type http://localhost:8080/hello into the browser)....it will show "Hi There" (the default)
   greeting = params[:greeting] || "Hi There"
+
+  #calling the 'erb' method with params index and locals...ruby doesn't make you use parens here
+  #This 'renders' the :index view
+  #The:locals={’greeting’=>greeting}part says, ”Also give this greet view ('the greet view' is the file views/index.erb) the local variables greeting with this setting."
   erb :index, :locals => {'greeting' => greeting}
 end
